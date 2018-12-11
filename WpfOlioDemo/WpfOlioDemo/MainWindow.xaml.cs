e2e9b425c151511f6496471386c98ec031f2a74f
Kalle <mikko.kalle.pakarinen@gmail.com>
2018-12-11 14:04:14 +0200

    Branch-test

--- WpfOlioDemo/WpfOlioDemo/MainWindow.xaml.cs
+++ WpfOlioDemo/WpfOlioDemo/MainWindow.xaml.cs
@@ -23,6 +23,33 @@ namespace WpfOlioDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            DataGrid();
+        }
+
+        private void DataGrid()
+        {
+            DataGridTextColumn textColumn1 = new DataGridTextColumn();
+            textColumn1.Binding = new Binding("CustomerNum");
+            DataGridTextColumn textColumn2 = new DataGridTextColumn();
+            textColumn2.Binding = new Binding("FirstName");
+            DataGridTextColumn textColumn3 = new DataGridTextColumn();
+            textColumn3.Binding = new Binding("LastName");
+            DataGridTextColumn textColumn4 = new DataGridTextColumn();
+            textColumn4.Binding = new Binding("Address");
+            DataGridTextColumn textColumn5 = new DataGridTextColumn();
+            textColumn5.Binding = new Binding("Phone");
+
+            textColumn1.Header = "Asiakasnumero";
+            dgCustomer.Columns.Add(textColumn1);
+            textColumn2.Header = "Etunimi";
+            dgCustomer.Columns.Add(textColumn2);
+            textColumn3.Header = "Sukunimi";
+            dgCustomer.Columns.Add(textColumn3);
+            textColumn4.Header = "Osoite";
+            dgCustomer.Columns.Add(textColumn4);
+            textColumn5.Header = "Puhelin";
+            dgCustomer.Columns.Add(textColumn5);
         }
 
         List<Customer> customers = new List<Customer>();
@@ -36,7 +63,12 @@ namespace WpfOlioDemo
             customer.Phone = txtBoxPhone.Text;
         
             customers.Add(customer);
-         
+
+            foreach (Customer oneCustomer in customers)
+            {
+                dgCustomer.Items.Add(oneCustomer);
+            }
+            customers.Clear();
         }
     }
 }
