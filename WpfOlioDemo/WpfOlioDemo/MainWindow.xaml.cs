d12d97514aabe8e695a1b957002fbdbcb81122a9
Kalle <mikko.kalle.pakarinen@gmail.com>
2018-12-14 19:15:58 +0200

    Beautifully code and UI

--- WpfOlioDemo/WpfOlioDemo/MainWindow.xaml.cs
+++ WpfOlioDemo/WpfOlioDemo/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
+
 
 namespace WpfOlioDemo
 {
@@ -22,36 +24,10 @@ namespace WpfOlioDemo
     {
         public MainWindow()
         {
-            InitializeComponent();
-
-            DataGrid();
+            InitializeComponent();         
         }
 
-        private void DataGrid()
-        {
-            DataGridTextColumn textColumn1 = new DataGridTextColumn();
-            textColumn1.Binding = new Binding("CustomerNum");
-            DataGridTextColumn textColumn2 = new DataGridTextColumn();
-            textColumn2.Binding = new Binding("FirstName");
-            DataGridTextColumn textColumn3 = new DataGridTextColumn();
-            textColumn3.Binding = new Binding("LastName");
-            DataGridTextColumn textColumn4 = new DataGridTextColumn();
-            textColumn4.Binding = new Binding("Address");
-            DataGridTextColumn textColumn5 = new DataGridTextColumn();
-            textColumn5.Binding = new Binding("Phone");
-
-            textColumn1.Header = "Asiakasnumero";
-            dgCustomer.Columns.Add(textColumn1);
-            textColumn2.Header = "Etunimi";
-            dgCustomer.Columns.Add(textColumn2);
-            textColumn3.Header = "Sukunimi";
-            dgCustomer.Columns.Add(textColumn3);
-            textColumn4.Header = "Osoite";
-            dgCustomer.Columns.Add(textColumn4);
-            textColumn5.Header = "Puhelin";
-            dgCustomer.Columns.Add(textColumn5);
-        }
-
+      
         List<Customer> customers = new List<Customer>();
         private void BtnAddCustomer_Click(object sender, RoutedEventArgs e)
         {
@@ -60,8 +36,7 @@ namespace WpfOlioDemo
             customer.FirstName = txtBoxFName.Text;
             customer.LastName = txtBoxLName.Text;
             customer.Address = txtBoxAddress.Text;
-            customer.Phone = txtBoxPhone.Text;
-        
+            customer.Phone = txtBoxPhone.Text;    
             customers.Add(customer);
 
             foreach (Customer oneCustomer in customers)
